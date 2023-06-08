b497c7d0c41960423df5f5a258deec744b952d95
Chinmoy Thakuria <112659063+chinmoy12345678@users.noreply.github.com>
2023-06-08 14:22:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import CoinDetails from './components/CoinDetails'
+import Coins from './components/Coins'
+import Exchanges from './components/Exchanges'
+import Home from './components/Home'
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/coins' element={<Coins />} />
+          <Route path='/exchanges' element={<Exchanges />} />
+          <Route path='/coin/:id' element={<CoinDetails />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
